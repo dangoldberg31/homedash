f09c2deebda495bc28261aca3344dc608c43f69e
dangoldberg31 <dan.goldberg@gmail.com>
2021-07-21 20:20:30 -0400

    try just air pollution data removed

--- homedash2/src/Components/forecastCurrently.js
+++ homedash2/src/Components/forecastCurrently.js
@@ -13,15 +13,15 @@ export const ForecastCurrently = ({convertUnixTime, convertUnixDate, convertUnix
                     
                 </div>
             </div>
-            {/* <div id="dataContainer" className="section">
+            <div id="dataContainer" className="section">
                 <div className="todaydata">
                     <p className="weatherData">Wind <br /><span className="data">{Math.round(forecast['current']['wind_speed'])} mph </span></p><br />
                     <p className="weatherData">Gusts <br /> <span className="data">{Math.round(forecast['hourly'][0]['wind_gust'])} mph</span></p>
                 </div>
-                <div className="todaydata">
+                {/* <div className="todaydata">
                     <p className="weatherData" id="airquality">Air Quality <br /><span className="data">{airQuality[1]}</span></p><br />
                     <p className="weatherData" id="airquality">PM2.5 <br /><span className="data">{airQuality[0]}</span></p>
-                </div>
+                </div> */}
                 <div className="todaydata" id="todaydatacenter">
                     <p className="weatherDatacenter" id="feelslike">Feels Like <br /><span className="datacenter">{Math.round(forecast['current']['feels_like'])} F</span></p><br />
                     <p className="weatherDatacenter" id="humidity">Humidity: <br /><span className="datacenter">{Math.round(forecast['current']['humidity'])}%</span></p>
@@ -34,7 +34,7 @@ export const ForecastCurrently = ({convertUnixTime, convertUnixDate, convertUnix
                     <p className="weatherData">Sunrise <br /><span className="data">{convertUnixTime(forecast['current']['sunrise']*1000)[0]+':'+convertUnixTime(forecast['current']['sunrise']*1000)[1]+' '+convertUnixTime(forecast['current']['sunrise']*1000)[2]}</span></p><br />
                     <p className="weatherData">Sunset <br /><span className="data">{convertUnixTime(forecast['current']['sunset']*1000)[0]+':'+convertUnixTime(forecast['current']['sunset']*1000)[1]+' '+convertUnixTime(forecast['current']['sunset']*1000)[2]}</span></p>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
